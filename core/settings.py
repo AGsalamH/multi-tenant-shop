e6f2f06e880207750ddf6edf5e6f1283a3f10507
Ahmed Gamal <agsalamh@gmail.com>
2024-04-27 10:58:19 +0300

    fix: remove unused custom middleware

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'core.middlewares.CustomMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
